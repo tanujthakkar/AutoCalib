1796e719c031a7b2ccf615d586ad5bbbd15ced99
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-04-06 13:54:46 -0400

    Initialized repo

---
+++ Code/Wrapper.py
@@ -0,0 +1,11 @@
+#!/usr/env/bin python3
+
+"""
+CMSC733 Spring 2022: Classical and Deep Learning Approaches for Geometric Computer Vision
+Homework 1: AutoCalib
+
+Author(s):
+Tanuj Thakkar (tanuj@umd.edu)
+M. Engg Robotics
+University of Maryland, College Park
+"""
