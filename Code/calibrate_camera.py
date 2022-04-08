da695d5942af0e0d1aacc262c37cb68c299eacd6
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-04-08 00:21:56 -0400

    Added optimize_params, bugs exist

--- Code/calibrate_camera.py
+++ Code/calibrate_camera.py
@@ -16,6 +16,7 @@ import os
 import numpy as np
 import time
 import argparse
+from scipy.optimize import least_squares
 
 from utils import *
 
@@ -26,12 +27,15 @@ class CalibrateCamera:
 
     def __init__(self, data_dir: str) -> None:
         self.img_set = create_image_set(data_dir)
+        self.M = None
+        self.m = None
         self.homography_set = list()
         self.K = None
         self.Rt = None
 
     def __estimate_homography_set(self, box_size: float, num_pts_x: int, num_pts_y: int) -> np.array:
 
+        m = np.empty([0,54,3])
         for img in self.img_set:
             img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             ret, corners = cv2.findChessboardCorners(img_gray, (num_pts_x, num_pts_y), None)
@@ -41,11 +45,14 @@ class CalibrateCamera:
             X = np.flip((X.reshape(54, 1) * box_size), axis=0)
             Y = (Y.reshape(54, 1) * box_size)
             M = np.float32(np.hstack((Y, X)))
+            M = np.hstack((M, np.ones([1, len(M)]).transpose()))
+            self.M = M
 
             if ret:
                 corners = corners.reshape(-1, 2)
                 corners = cv2.cornerSubPix(img_gray, corners, (11, 11), (-1, -1), (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.01))
-                # corners = cv2.cornerSubPix(img_gray, corners, (5, 5), (-1, -1), (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.0001))
+                corners = np.hstack((corners, np.ones([1, len(corners)]).transpose()))
+                m = np.insert(m, len(m), corners, axis=0)
 
                 for corner in corners:
                     cv2.circle(img, (int(corner[0]), int(corner[1])), 2, (0,255,0), 2)
@@ -53,6 +60,7 @@ class CalibrateCamera:
                 H = cv2.findHomography(M, corners)[0]
                 self.homography_set.append(H)
 
+        self.m = m
         self.homography_set = np.array(self.homography_set)
         return self.homography_set
 
@@ -88,7 +96,7 @@ class CalibrateCamera:
                       [0, 0, 1]])
 
         self.K = K
-        print(K)
+        print("K:\n", K)
         return K
 
     def estimate_extrinsics_set(self) -> np.array:
@@ -96,13 +104,21 @@ class CalibrateCamera:
         def estimate_extrinsics(homography: np.array, K: np.array) -> np.array:
 
             K_inv = np.linalg.inv(K)
-            lambda_ = 1/np.linalg.norm(np.dot(K_inv, homography[:,0]))
+            lambda_ = 1/np.linalg.norm(np.dot(K_inv, homography[:,0]), ord=2)
 
-            Rt = np.zeros([3,4])
-            Rt[:,0] = lambda_ * np.dot(K_inv, homography[:,0])
-            Rt[:,1] = lambda_ * np.dot(K_inv, homography[:,1])
-            Rt[:,2] = np.matmul(Rt[:,0], Rt[:,1])
-            Rt[:,3] = lambda_ * np.dot(K_inv, homography[:,2])
+            # Rt = np.zeros([3,4])
+
+            R = np.zeros([3,3])
+            R[:,0] = lambda_ * np.dot(K_inv, homography[:,0])
+            R[:,1] = lambda_ * np.dot(K_inv, homography[:,1])
+            R[:,2] = np.cross(R[:,0], R[:,1])
+            t = (lambda_ * np.dot(K_inv, homography[:,2])).reshape(3,1)
+
+            Q = np.array([R[:,0], R[:,1], R[:,2]]).transpose()
+            U, S, Vt = np.linalg.svd(Q)
+            R = np.dot(U, Vt)
+
+            Rt = np.hstack([R, t])
 
             return Rt
 
@@ -110,4 +126,36 @@ class CalibrateCamera:
         for homography in self.homography_set:
             Rt = np.insert(Rt, len(Rt), estimate_extrinsics(homography, self.K), axis=0)
 
-        print(Rt, Rt.shape)
+        self.Rt = Rt
+        return Rt
+
+    def optimize_params(self):
+
+        def projection_error(m: np.array, M: np.array, K: list, Rt: np.array):
+            K = np.array([[K[0], K[1], K[2]],
+                          [0, K[3], K[4]],
+                          [0, 0, 1]])
+            Rt = np.asarray([Rt[:,0], Rt[:,1], Rt[:,3]]).transpose()
+            m_ = np.matmul(Rt, M)
+            m_ = m_/m_[-1]
+            m_ = np.dot(K, m_)
+
+            return np.linalg.norm(np.subtract(m, m_), ord=2)
+
+        def projection_loss(params: list, M: np.array, Rt: np.array) -> float:
+
+            loss = 0
+            for i, corners in enumerate(self.m):
+                for j, corner in enumerate(corners):
+                    loss += projection_error(corner, M[j], params[:5], Rt[i])
+
+            return loss
+
+        print(self.Rt[0])
+        # x = least_squares(fun=projection_loss, x0=[self.K[0,0], self.K[0,1], self.K[1,2], self.K[1,1], self.K[1,2], 0, 0], args=[self.M, self.Rt])
+        # print(x)
+
+    def calibrate(self):
+        self.estimate_intrinsics()
+        self.estimate_extrinsics_set()
+        self.optimize_params()
